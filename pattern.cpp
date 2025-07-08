40c14ac348e258811448e5d7a79e219423f75dbc
xyz.gmail.com <jasraj0504.becse24@chitkara.edu.in>
2025-07-08 09:17:55 +0530

    Added New Files

---
+++ pattern.cpp
@@ -0,0 +1,48 @@
+// #include<iostream>                                              //Pattern to print square using "*"
+// int main(){
+//   int n;
+//   std::cout<<"Enter the number of rows ";
+//   std::cin>>n;
+// for(int i=1;i<=n;i++){
+//    for(int j=1;j<=n;j++){
+//       std::cout<<"*";
+//    }std::cout<<"\n";
+// }
+//   return 0;
+// }
+// #include<iostream>                                    //Right Angled Trianle using "*"
+// int main(){ 
+//   int n;
+//   std::cout<<"Enter the number of rows ";
+//   std::cin>>n;
+// for(int i=1;i<=n;i++){
+//    for(int j=1;j<=i;j++){
+//       std::cout<<"*";
+//    }std::cout<<"\n";
+// }
+//   return 0;
+// }
+// #include<iostream>                                   //Right Angled Trianle Using Numbers
+// int main(){
+//     int n;
+//     std::cout<<"Enter the Number of rows ";
+//     std::cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             std::cout<<j<<" ";
+//         }std::cout<<"\n";
+//     }
+//     return 0;
+// }
+// #include<iostream>                                    //Inverted Right Angled Trianle to print the numbers
+// int main(){
+//     int n;
+//     std::cout<<"Enter the number of rows ";
+//     std::cin>>n;
+//     for(int i=n;i>=1;i--){
+//         for(int j=1;j<=i;j++){
+//             std::cout<<j<<" ";                         //std::cout<<"*" to print the pattern in stars
+//         }std::cout<<"\n";
+//     }
+//     return 0;
+// }
---
+++ switch.cpp
@@ -0,0 +1,52 @@
+// #include<iostream>
+// int main(){
+//     char exampleVar;
+//     int a,b;
+//     std::cout<<"Enter an operator ( +,-,*,/) ";
+//     std::cin>>exampleVar;
+
+//     std::cout<<"Enter the numbers ";
+//     std::cin>>a>>b;
+
+//     switch(exampleVar){
+//         case '+':
+//         std::cout<<a+b<<"\n";
+//         break;
+
+//         case '-':
+//         std::cout<<a-b<<"\n";
+//         break;
+
+//         case '*':
+//         std::cout<<a*b<<"\n";
+//         break;
+
+//         case '/':
+//         if(b!=0)
+//             std::cout<<a/b<<"\n";
+//             else
+//             std::cout<<"Cannot Divide By Zero"<<"\n";
+//             break;
+        
+//         default:
+//         std::cout<<"Invalid Operator Entered.\n ";
+//     }
+//     return 0;
+// }
+// #include<iostream>
+// int main(){
+// int game;
+// std::cout<<"Enter the Choice 1)play game 2)Graphic Settings 3)Exit ";
+// std::cin>>game;
+// switch(game){
+//     case 1: std::cout<<"Game Started"<<"\n";
+//     break;
+//     case 2: std::cout<<"Loading Settings"<<"\n";
+//     break;
+//     case 3: std::cout<<"Quiting Game"<<"\n";
+//     break;
+//     default:
+//     std::cout<<"Please Choose Correct Option \n";
+// }
+// return 0;
+// }
